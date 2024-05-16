8a12aea074b595a24c43940dd28d2aceade113b5
Willame Mouzinho <willame.dev@gmail.com>
2024-05-16 09:48:17 -0300

    fix: change name from facade

--- structurals/facade/main-app.ts
+++ structurals/facade/main-app.ts
@@ -1,4 +1,4 @@
 import { BuyCryptoFacade } from "./buy-crypto-facade";
 
 const buyCrypto = new BuyCryptoFacade();
-buyCrypto.buyCrypyocurrency(1000, "BTC");
+buyCrypto.buyCryptocurrency(1000, "BTC");
